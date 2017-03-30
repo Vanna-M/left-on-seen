ae939b6aafafdea6dda972e02957ea78f2703c73
JiaqiGao <jiaqigao12@gmail.com>
2017-03-29 22:46:44 -0400

    added js and html

--- static/index.js
+++ static/index.js
@@ -1,19 +1,24 @@
-var jsonCircles = [
-    "Mercury": [ "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" ],
-    "Venus": [ "x_axis": 130, "y_axis": 130, "radius": 50, "color" : "red" ]
+var svg = d3.select("#svg");
+
+var planets = [
+    {"name":"Mercury", "size": 20, "orbpd": 88, "x_axis": 30, "y_axis": 100, "color":"red"},
+    {"name":"Venus", "size": 20, "orbpd": 88, "x_axis": 60, "y_axis": 100, "color":"red"},
+    {"name":"Earth", "size": 20, "orbpd": 88, "x_axis": 90, "y_axis": 100, "color":"red"},
+    {"name":"Mars", "size": 20, "orbpd": 88, "x_axis": 120, "y_axis": 100, "color":"red"},
+    {"name":"Jupiter", "size": 20, "orbpd": 88, "x_axis": 150, "y_axis": 100, "color":"red"},
+    {"name":"Saturn", "size": 20, "orbpd": 88, "x_axis": 180, "y_axis": 100, "color":"red"},
+    {"name":"Uranus", "size": 20, "orbpd": 88, "x_axis": 210, "y_axis": 100, "color":"red"},
+    {"name":"Neptune", "size": 20, "orbpd": 88, "x_axis": 240, "y_axis": 100, "color":"red"}
 ];
 
-var svgContainer = d3.select("body").append("svg")
-                                     .attr("width", 200)
-                                     .attr("height", 200);
 
-var circles = svgContainer.selectAll("circle")
-                          .data(jsonCircles)
-                          .enter()
-                          .append("circle");
+var circles = svg.selectAll("circle")
+    .data(planets)
+    .enter()
+    .append("circle");
 
 var circleAttributes = circles
-                       .attr("cx", function (d) { return d.x_axis; })
-                       .attr("cy", function (d) { return d.y_axis; })
-                       .attr("r", function (d) { return d.radius; })
-                       .style("fill", function(d) { return d.color; });
+    .attr("cx", function (d) { return d.x_axis; })
+    .attr("cy", function (d) { return d.y_axis; })
+    .attr("r", function (d) { return d.size; })
+    .style("fill", function(d) { return d.color; });
