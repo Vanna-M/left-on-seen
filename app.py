57b9f3ce4b4e2fa658761e5dfe9e2426ee21c6e5
Billy Wong <bwong5@stuy.edu>
2017-03-29 22:18:13 -0400

    rename convertcsv.csv to planets.csv and added data reading

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
@@ -6,7 +7,29 @@ app = Flask(__name__)
 def root():
     #rotPds = list of rotational periods for all of the planets
     #rockets: dict of all rocket launches, saved as: {date:destination}
-    return render_template("index.html",rotPds=[""],rockets={"date":"dest"})
+    rotPds = get_rotPds()
+    rockets = get_rockets()
+    return render_template("index.html",rotPds=rotPds,rockets={"date":"dest"})
+
+
+def get_rotPds():
+    f = open('data/planets.csv','r')
+    read = csv.reader(f)
+    pds = []
+    for row in read:
+        if row[0] == 'Rotation Period (hours)':
+            pds = row
+    f.close()
+    return pds
+
+
+def get_rockets():
+    f = open('data/rockets.csv','r')
+    read = csv.reader(f)
+    rockets = {}
+    for row in read:
+        rockets[row[1]] = row[2]
+    return rockets
 
 if __name__ == '__main__':
     app.debug = True
